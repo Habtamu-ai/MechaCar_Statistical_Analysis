0cd5a48f8024e12b14a187d7ea14429fac579986
Habtamu Molla <78656720+Habtamu-ai@users.noreply.github.com>
2021-06-07 13:45:18 -0400

    Final do file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -24,3 +24,16 @@ total_summary <- Suspension  %>%
 #4. Group by_lot_summary
 group_total_summary <- Suspension %>% group_by(Manufacturing_Lot) %>%
   summarise(Mean_PSI = mean(PSI), Median_PSI = median(PSI),Variance_PSI = var(PSI),Stdev_PSI =sd(PSI))
+
+#Deliverable 3
+### T_TEST
+?t.test()
+t.test(x=Suspension$PSI, mu=1500) # t test n all cars' PSI with a mean of 1500
+pop1 <- subset(Suspension, Manufacturing_Lot=='Lot1') # Create subset for lot 1 cars
+t.test(x=pop1$PSI, mu=1500) # t test on lot 1 cars' PSI with a mean of 1500
+pop2 <- subset(Suspension, Manufacturing_Lot=='Lot2') # Create subset for lot 2 cars
+t.test(x=pop2$PSI, mu=1500) # t test on lot 2 cars' PSI with a mean of 1500
+pop3 <- subset(Suspension, Manufacturing_Lot=='Lot3') # Create subset for lot 3 cars
+t.test(x=pop3$PSI, mu=1500) # t test on lot 3 cars' PSI with a mean of 1500
+
+
